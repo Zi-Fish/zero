d34f3a83b38ed9805486a8880113d9c35539e732
lihanxi <3138220743@qq.com>
2022-05-23 21:04:24 +0800

    2022052301

---
+++ ajax/2.1server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/home', (request, response)=>{
+    //响应一个页面
+    response.sendFile(__dirname + '/index.html');
+});
+
+app.get('/data', (request, response)=>{
+    response.send('用户数据');
+});
+
+app.listen(8001, ()=>{
+    console.log("8001服务已经启动...");
+});
