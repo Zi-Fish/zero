0c1b19c2bfa3da513e0380e7168f08b05d1a8806
lihanxi <3138220743@qq.com>
2022-05-28 21:43:39 +0800

    2022052801

---
+++ vue/htm/单文件组件/School.vue
@@ -0,0 +1,37 @@
+<template>
+	<div class="demo">
+		<h2>学校名称：{{name}}</h2>
+		<h2>学校地址：{{address}}</h2>
+		<button @click="showName">点我提示学校名</button>	
+	</div>
+</template>
+
+<script>
+import vue from '../vue'
+
+//向外暴露
+// export const school=vue.extend({})分别暴露
+// const school=vue.extend({})//统一暴露（前）
+	 export default {
+		name:'School',
+		data(){
+			return {
+				name:'尚硅谷',
+				address:'北京昌平'
+			}
+		},
+		methods: {
+			showName(){
+				alert(this.name)
+			}
+		},
+	}
+	// export {school} 统一暴露（后）
+	// export default school 默认暴露
+</script>
+
+<style>
+	.demo{
+		background-color: orange;
+	}
+</style>
---
+++ vue/htm/单文件组件/main.js
@@ -0,0 +1,7 @@
+import App from './App.vue'
+// 入口文件
+new Vue({
+	el:'#root',
+	template:`<App></App>`,
+	components:{App},
+})
