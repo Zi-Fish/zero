20a5b4ef81da2ec8b586254fd9e4f91049f48ef2
lihanxi <3138220743@qq.com>
2022-06-26 21:40:28 +0800

    2022062601

---
+++ vue/srcc/src-17-过渡与动画/components/Test3.vue
@@ -0,0 +1,36 @@
+<template>
+<!-- 第三方库，animate.css -->
+	<div>
+		<button @click="isShow = !isShow">显示/隐藏</button>
+		<transition-group 
+			appear
+			name="animate__animated animate__bounce" 
+			enter-active-class="animate__swing"
+			leave-active-class="animate__backOutUp"
+		>
+		<!-- 都得写一个key值 -->
+			<h1 v-show="!isShow" key="1">你好啊！</h1>
+			<h1 v-show="isShow" key="2">子释！</h1>
+		</transition-group>
+	</div>
+</template>
+
+<script>
+	import 'animate.css'
+	export default {
+		name:'Test',
+		data() {
+			return {
+				isShow:true
+			}
+		},
+	}
+</script>
+
+<style scoped>
+	h1{
+		background-color: orange;
+	}
+	
+
+</style>
--- vue/thefs/vue.config.js
+++ vue/thefs/vue.config.js
@@ -1,5 +1,20 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  lintOnSave:false
+  lintOnSave:false,
+  // 开启代理服务器（1）
+  // 只能配置一个代理，不能灵活配置走不走代理
+// devServer: {
+//   proxy:'http://localhost:5000'
+// },
+devServer: {
+  proxy: {
+    // 请求前缀api 走代理
+    '/api': {
+      target: 'http://localhost:5000',
+      ws: true,
+      changeOrigin: true
+    }
+  }
+}
 })
