7c247d40e34eddab7152cb18066182fbbb7338b0
lihanxi <3138220743@qq.com>
2022-06-28 13:42:54 +0800

    2022062801

---
+++ vue/srcc/src-20-github搜索案例/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container">
+<Search/>
+<List/>
+</div>
+</template>
+
+<script>
+import List from './compoments/List.vue';
+import Search from './compoments/Search.vue';
+// import './assets/css/bootstrap.css'
+export default {
+  components: {List,Search},
+  name: "App",
+};
+</script>
---
+++ vue/srcc/src-21-vue-resource/compoments/Search.vue
@@ -0,0 +1,46 @@
+<template>
+  <section class="jumbotron">
+    <h3 class="jumbotron-heading">Search Github Users</h3>
+    <div>
+      <input
+        type="text"
+        placeholder="enter the name you search"
+        v-model="keywords"
+      />&nbsp;<button @click="searchUsers">Search</button>
+    </div>
+  </section>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  name: "Search",
+  data() {
+    return {
+      keywords: "",
+    };
+  },
+  methods: {
+    searchUsers() {
+      // 请求前更新list数据
+      this.$bus.$emit("updateListData", {isFirst:false,isLoading:true,errMsg:'',users:[]});
+      // 当作模板去解析，用反引号
+      // axios.get(`https://api.github.com/search/users?q=${this.keywords}`).then
+       this.$http.get(`https://api.github.com/search/users?q=${this.keywords}`).then
+      (
+        (response) => {
+          console.log("请求成功", response.data.items);
+          this.$bus.$emit("updateListData", {isLoading:true,errMsg:'', users:response.data.items});
+        },
+        (error) => {
+          console.log("请求失败", error.message);
+          this.$bus.$emit("updateListData", {isLoading:false,errMsg:message, users:[]});
+        }
+      );
+    },
+  },
+};
+</script>
+
+<style>
+</style>
